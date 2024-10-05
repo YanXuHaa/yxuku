5f74cb58b40558f8d8fb4133846af719abd0751a
YanXuHaa <92002483+YanXuHaa@users.noreply.github.com>
2024-10-05 22:10:48 +0800

    Update 酒馆.sh

--- 酒馆.sh
+++ 酒馆.sh
@@ -2,15 +2,15 @@
 
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list && apt update && apt upgrade
 
-pkg install openssl wget htop tsu proot git nano neofetch python curl cmake esbuild
+pkg install -y openssl wget htop tsu proot git nano neofetch python curl cmake esbuild
 
-apt update && apt upgrade
+pkg update && pkg upgrade && apt update -y && apt upgrade -y
 
 git clone https://gitclone.com/github.com/SillyTavern/SillyTavern
 
 cd SillyTavern
 
-pkg install nodejs
+pkg install -y nodejs
 
 npm config set registry https://registry.npmmirror.com
 
